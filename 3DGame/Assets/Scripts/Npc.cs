256ba513f4ca3a6462e1bd63c7bf3ec15025cb10
stiffgg <71679936+stiffgg@users.noreply.github.com>
2021-01-19 12:30:43 +0800

    1/19 END

---
+++ 3DGame/Assets/Scripts/Npc.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+public class Npc : MonoBehaviour
+{
+    [Header("NPC資料")]
+    public NpcData data;
+    [Header("對話框")]
+    public GameObject dialog;
+    [Header("對話內容")]
+    public Text textContent;
+    [Header("對話間隔")]
+    public float interval = 0.2f;
+    [Header("對話者名稱")]
+    public Text textName;
+
+    public bool playerInArea;
+    public enum NPCState
+    {
+      FirstDialog, Missioning, Finsh
+    }
+    public NPCState state = NPCState.FirstDialog;
+
+
+    /*private void Start()
+    {
+        StartCoroutine(Test());
+    }
+    private IEnumerator Test()
+    {
+        print("嗨");
+        yield return new WaitForSeconds(1.5f);
+
+    }
+    public enum NPCstate { }*/
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "芭比")
+        {
+            playerInArea = true;
+            StartCoroutine(Dialog());
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "芭比")
+        {
+            playerInArea = false;
+            StopDialog();
+        }
+       
+    }
+    private void StopDialog()
+    {
+        dialog.SetActive(false);
+        StopAllCoroutines();
+    }
+    private IEnumerator Dialog()
+    {
+        dialog.SetActive(true);
+        textContent.text = "";
+        textName.text = name;
+
+        string dialogString = data.dialogA;
+
+        switch (state)
+        {
+            case NPCState.FirstDialog:
+                dialogString = data.dialogA;
+                break;
+            case NPCState.Missioning:
+                dialogString = data.dialogB;
+                break;
+            case NPCState.Finsh:
+                dialogString = data.dialogC;
+                break;
+            
+                
+        }
+
+        for (int i = 0; i < dialogString.Length; i++)
+        {
+            textContent.text += dialogString[i]+"";
+            yield return new WaitForSeconds(interval);
+        }
+    
+    }
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+}
---
+++ 3DGame/Assets/Scripts/NpcData.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+[CreateAssetMenu(fileName ="NPC資料", menuName ="Stiff/NPC資料")]
+public class NpcData : ScriptableObject
+
+{
+    [Header("第一段對話"), TextArea(1, 5)]
+    public string dialogA;
+    [Header("第二段對話"), TextArea(1, 5)]
+    public string dialogB;
+    [Header("第三段對話"), TextArea(1, 5)]
+    public string dialogC;
+    [Header("任務項目需求數量")]
+    public int count;
+    [Header("已經取得項目數量")]
+    public int countCurrent;
+}
